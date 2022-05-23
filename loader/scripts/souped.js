988a7ce023344a3dd4c857bf36637fb3c8a96e18
BowDown097 <42720004+BowDown097@users.noreply.github.com>
2022-05-23 06:33:54 -0700

    Add CSV patcher

--- loader/scripts/souped.js
+++ loader/scripts/souped.js
@@ -11,7 +11,38 @@
 
 console.log("Welcome to souped.js!");
 
+const csvRegex = new RegExp(`\\s*(")?(.*?)\\1\\s*(?:,|$)`, "gs");
+const match = line => {
+    const matches = [...line.matchAll(csvRegex)].map(m => m[2]);
+    matches.pop();
+    return matches;
+}
+
 //Patcher helper functions
+souped.registerCsvPatcher = function (callback, filename) {
+    souped.registerPatcher((name, data) => {
+        const lines = data.split("\n");
+        const headers = match(lines.shift());
+
+        const dataObj = lines.map(line => {
+            return match(line).reduce((acc, cur, i) => {
+                const val = cur.length <= 0 ? null : Number(cur) || cur;
+                const key = headers[i] ?? `extra_${i}`;
+                return { ...acc, [key]: val };
+            }, {});
+        });
+
+        var { successful, data } = callback(dataObj);
+        if (successful) {
+            const keys = [...new Set(data.map(e => Object.keys(e)).flat())];
+            const patchedStr = `${keys.join(',')}\n` + data.map(e => Object.values(e).join(',')).join('\n');
+            return { successful: successful, data: patchedStr };
+        }
+
+        return { successful: false, data: data };
+    }, filename);
+}
+
 souped.registerJsonPatcher = function (callback, filename) {
     souped.registerPatcher((name, data) => {
         var dataObj = JSON.parse(data);
