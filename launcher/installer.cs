7284aa5184e79c504098325cb53b6f2c8bd97647
DisabledMallis <mallisiscool@gmail.com>
2022-06-16 08:55:16 -0400

    oooo aahhh

---
+++ launcher/installer.cs
@@ -0,0 +1,12 @@
+namespace SoupedModFramework
+{
+    using System;
+
+    public class Installer
+    {
+        public static void InstallGame()
+        {
+
+        }
+    }
+}
--- launcher/main.cs
+++ launcher/main.cs
@@ -12,9 +12,31 @@ namespace SoupedModFramework
         {
             Console.WriteLine("Loading SMF Launcher...");
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new WebView());
+            Console.WriteLine("1) Verify/Install game from Steam");
+            Console.WriteLine("2) Patch game files for mod loading (requires an install)");
+            Console.WriteLine("3) Run SoupedModFramework");
+            bool chosen = false;
+            while (!chosen)
+            {
+                Console.Write("> ");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+
+                        chosen = true;
+                        break;
+                    case "2":
+                        chosen = true;
+                        break;
+                    case "3":
+                        chosen = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option, try again");
+                        break;
+                }
+            }
         }
     }
 }
